50a2a4e02e1aabae1df14b9903404bce6f16fc1b
Malakaka <malakalsenine@gmail.com>
2026-07-01 14:42:47 +0400

    add in Review class The appropriate data annotation

--- solution01/project01/Models/Review.cs
+++ solution01/project01/Models/Review.cs
@@ -12,16 +12,29 @@ namespace project01.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int reviewId { get; set; }  //generated 
+        public int reviewId { get; set; }  //generated (Primary Key)
 
+
+        [Required]
+        [ForeignKey ("User")]
         public int userId { get; set; } // user input (Foreign Key)
 
+
+        [Required]
+        [ForeignKey("Product")]
         public int productId { get; set; } // user input (Foreign Key)
 
+
+        [Required]
+        [Range (1 , 5)]
         public int rating { get; set; } // user input
 
-        public string comment { get; set; } // user input
 
+        [MaxLength (1000)]
+        public string? comment { get; set; } // user input
+
+
+        [Required]
         public DateTime reviewDate { get; set; } // user input
 
     }
