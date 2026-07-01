4fa94e536bcd3adfe14466de52a13f00142a8c2b
Malakaka <malakalsenine@gmail.com>
2026-07-01 14:31:20 +0400

    add in product class The appropriate data annotation

--- solution01/project01/Models/Product.cs
+++ solution01/project01/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,42 @@ namespace project01.Models
 {
     public class Product
     {
+        [Key]
+        public int productId { get; set; }  //generated (Primary Key)
 
-        public int productId { get; set; }  //generated 
 
+        [Required]
+        [MaxLength(150)]
         public string productName { get; set; } // user input
 
-        public string description { get; set; } // user input
 
+
+        [MaxLength(1000)]
+        public string? description { get; set; } // user input
+
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
         public decimal price { get; set; } // user input
 
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
         public int stockQuantity { get; set; } // user input
 
-        public string imageUrl { get; set; } // user input
 
-        public int  categoryId { get; set; } // user input
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // user input
 
-        public DateTime registrationDate { get; set; } // user input (Foreign Key)
 
-        public bool isAvailable { get; set; } // Default
+        [Required]
+        public int  categoryId { get; set; } // user input (Foreign Key)
+
+
+        [Required]
+        public DateTime createdAt { get; set; } // user input (Foreign Key)
+
+        public bool isAvailable { get; set; } = true; // Default
 
     }
 }
