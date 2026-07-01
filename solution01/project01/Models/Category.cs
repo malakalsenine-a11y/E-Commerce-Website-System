42e4ad718e4240229aafe1ab2838d4e0d2f6fba7
Malakaka <malakalsenine@gmail.com>
2026-07-01 12:29:39 +0400

    Create Category class and add (attribute, data type)

--- solution01/project01/Models/Category.cs
+++ solution01/project01/Models/Category.cs
@@ -8,5 +8,10 @@ namespace project01.Models
 {
     public class Category
     {
+        public int categoryId { get; set; }  //generated 
+        public string categoryName { get; set; } // user input
+        public string description { get; set; } // user input
+
+        public string imageUrl { get; set; } // user input
     }
 }
