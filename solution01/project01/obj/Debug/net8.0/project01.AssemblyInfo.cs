fd87ff2cca74d67768a6e1a85dad4d88d8a6c177
Malakaka <malakalsenine@gmail.com>
2026-07-01 12:22:23 +0400

    create user class

--- solution01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ solution01/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d10367d674287395dc0c607df73d602dfc716e")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
