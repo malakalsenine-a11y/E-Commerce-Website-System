c4293a852ae3e18a2f20364686016c5c2a2a7c5c
Malakaka <malakalsenine@gmail.com>
2026-07-01 14:21:20 +0400

    add in user class The appropriate data annotation

--- solution01/project01/Models/User.cs
+++ solution01/project01/Models/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,21 +10,43 @@ namespace project01.Models
 {
     public class User
     {
-        public int userId { get; set; }  //generated 
+        [Key]
+        public int userId { get; set; }  //generated (Primary Key)
+
+        [Required]
+        [MaxLength (50)]
+
         public string username { get; set; } // user input
+
+        [Required]
+        [MaxLength(150)]
         public string email { get; set; } // user input
 
+
+        [Required]
+        [MaxLength(256)]
         public string passwordHash { get; set; } // user input
 
+
+        [Required]
+        [MaxLength(100)]
         public string fullName { get; set; } // user input
 
-        public string phoneNumber { get; set; } // user input
 
-        public string address { get; set; } // user input
+        [MaxLength(20)]
+        public string? phoneNumber { get; set; } // user input
 
+
+        [MaxLength(20)]
+        public string? address { get; set; } // user input
+
+
+        [Required]
         public DateTime registrationDate { get; set; } // user input
 
-        public bool isActive { get; set; } // Default
+
+
+        public bool isActive { get; set; } = true ; // Default
 
 
 
