cd0eebe4d9734e7ee5e8eb9c0f42eb53e771ba74
Malakaka <malakalsenine@gmail.com>
2026-07-01 15:08:15 +0400

    Update // default value class The appropriate data annotation

--- solution01/project01/Models/Order.cs
+++ solution01/project01/Models/Order.cs
@@ -22,18 +22,18 @@ namespace project01.Models
 
 
         [Required]
-        public DateTime orderDate { get; set; } // user input
+        public DateTime orderDate { get; set; } // // system generated
 
 
         [Required]
-        [Range(0.01, double.MaxValue)]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
         public decimal totalAmount { get; set; } // user input
 
 
 
         [Required]
         [MaxLength (30)]
-        public string status { get; set; } = "Pending";// defult
+        public string status { get; set; } = "Pending";  // default value
 
 
         [Required]
@@ -45,6 +45,8 @@ namespace project01.Models
         [MaxLength(50)]
         public string paymentMethod { get; set; } // user input
 
+        // ========================================================
 
+        public User user { get; set; } //navigation
     }
 }
